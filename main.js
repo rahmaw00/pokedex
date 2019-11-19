f22a01d43a224843263855f59cc348c5109a30e4
Ramata Williams Bah <rwilliamsbah@nyc.yearup.org>
2019-11-19 16:52:42 -0500

    did more styling for the individual pokemon pages, and the intro page. building my color palette

--- main.js
+++ main.js
@@ -1,50 +1,24 @@
-class Ramata {
+class Pokemon {
   constructor(name) {
     this.name = name
-  }
-}
-
-class Stat {
-  constructor(name) {
-    this.name = undefined
     this.hp = null
     this.atk = null
     this.def = null
-    this.spd = null
-    this.total = null
   }
 }
 
-class Blaziken extends Stat {
-  constructor(name) {
-    super(name)
-    this.hp = 80
-    this.atk = 120
-    this.def = 70
-    this.spd = 80
-    this.total = {
-      total: this.hp + this.atk + this.def + this.spd
-    }
+class Trainer {
+  constructor() {
+    this.name = 'Ramata'
+    this.team = []
   }
-}
-console.log(this.total)
 
-class Mewtwo extends Stat {
-  constructor(name) {
-    super(name)
-    this.hp = 106
-    this.atk = 110
-    this.def = 90
-    this.spd = 130
+  addPokemon(name) {
   }
-}
 
-class Pikachu extends Stat {
-  constructor(name) {
-    super(name)
-    this.hp = 35
-    this.atk = 55
-    this.def = 40
-    this.spd = 90
+  fetchPokemon =  async () => {
+    let mewtwo = await fetch('https://fizal.me/pokeapi/api/v2/id/150.json').JSON()
+    let blaziken = await fetch('https://fizal.me/pokeapi/api/v2/id/257.json').JSON()
+    let pikachu = await fetch('https://fizal.me/pokeapi/api/v2/id/250.json').JSON()
   }
 }
